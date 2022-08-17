87b21de85ff60836879785861e20ee59d440ddf3
Rishi Srivastava <official.rishisrivastava@example.com>
2022-08-18 00:14:45 +0530

    login layout done

---
+++ app/src/main/java/com/rsstudio/spinny/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.rsstudio.spinny.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rsstudio.spinny.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
