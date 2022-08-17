50b33784b431364dcfef4f4161383b5c28823967
Rishi Srivastava <official.rishisrivastava@example.com>
2022-08-17 22:17:36 +0530

    initial commit

---
+++ app/src/main/java/com/rsstudio/spinny/ui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rsstudio.spinny.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rsstudio.spinny.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
