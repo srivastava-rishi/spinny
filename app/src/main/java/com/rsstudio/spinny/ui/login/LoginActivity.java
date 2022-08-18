5295755f7ee7e26c434e5674aff1a331d4a74031
Rishi Srivastava <official.rishisrivastava@example.com>
2022-08-18 14:14:35 +0530

    signup page done

--- app/src/main/java/com/rsstudio/spinny/ui/login/LoginActivity.java
+++ app/src/main/java/com/rsstudio/spinny/ui/login/LoginActivity.java
@@ -1,16 +1,20 @@
 package com.rsstudio.spinny.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.rsstudio.spinny.R;
+import com.rsstudio.spinny.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
